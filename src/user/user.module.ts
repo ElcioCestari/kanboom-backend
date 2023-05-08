f3a9cf9a38aae763d72d9b089109f81108a14f50
ElcioCestari <elciotaira@gmail.com>
2023-05-08 17:36:05 -0300

    feat(feat/mongo): add mapper

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import UserRepository from './repositories/user.repository';
 import { DatabaseModule } from 'src/database/database.module';
+import UserMapper from './mapper/user.mapper';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UserMapper],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,39 +1,35 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto ';
-import UserResponseDto from './dto/user-response.dto';
-import { User } from './entities/user.entity';
+import UserMapper from './mapper/user.mapper';
 import UserRepository from './repositories/user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly repository: UserRepository) {}
+  constructor(
+    private readonly repository: UserRepository,
+    private readonly mapper: UserMapper,
+  ) {}
 
   async login(user: LoginUserDto) {
     const response = await this.repository.findByEmailAndPassword(
-      user.email,
-      user.password,
+      user?.email,
+      user?.password,
     );
     if (!response) {
       throw new NotFoundException();
     }
-    return response;
+    return this.mapper.toResponse(response);
   }
 
-  create(dto: CreateUserDto) {
-    const entity = new User();
-    entity.email = dto.login;
-    entity.password = dto.password;
-    return this.repository.save(entity);
+  async create(dto: CreateUserDto) {
+    const entity = this.mapper.toEntity(dto);
+    const user = await this.repository.save(entity);
+    return this.mapper.toResponse(user);
   }
 
   async findAll() {
     const list = await this.repository.findAll();
-    return list.map((user) => {
-      const response = new UserResponseDto();
-      response.email = user.email;
-      response.id = user.id;
-      return response;
-    });
+    return list.map((user) => this.mapper.toResponse(user));
   }
 }
