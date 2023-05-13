eb18de3ea7ffe283b4f5665cdff8cc773278100e
ElcioCestari <elciotaira@gmail.com>
2023-05-13 12:42:03 -0300

    feat(feat/board): init board

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, BoardModule],
   controllers: [],
   providers: [],
 })
